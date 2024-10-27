19414b495c378bbfbae80515ae40ac0e278d0fc9
devyumi <143242666+devyumi@users.noreply.github.com>
2024-10-27 21:22:28 +0900

    Vue.js Composition API를 활용한 Component 연습

---
+++ vue/product-vue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue';
+import ProductList from './components/ProductList.vue';
+
+const products = ref([
+  { id: 1, name: '바나나', price: 1000, quantity: 20 },
+  { id: 2, name: '오렌지', price: 2000, quantity: 5 },
+  { id: 3, name: '딸기', price: 5000, quantity: 1 },
+  { id: 4, name: '블루베리', price: 3000, quantity: 30 },
+  { id: 5, name: '사과', price: 3200, quantity: 180 }
+])
+</script>
+
+<template>
+    <ProductList :productList="products" />
+</template>
