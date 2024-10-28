2df08295a995cce61047e85519b4db7d4ee54948
devyumi <143242666+devyumi@users.noreply.github.com>
2024-10-28 23:31:44 +0900

    Vue.js methods event computed로 변환

---
+++ vue/temperature-vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const c = ref(0)
+
+const f = computed({
+  get: () => c.value * (9 / 5) + 32,
+  set: (value) => {
+    c.value = (value - 32) * (5 / 9)
+  }
+})
+</script>
+
+<template>
+  <input type="number" v-model="c"> Celsius =
+  <input type="number" v-model="f"> Fahrenheit
+</template>
